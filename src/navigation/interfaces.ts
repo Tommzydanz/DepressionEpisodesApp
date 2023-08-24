ac86416516dd1f9b77c4fdf7156805f77108a657
samuelmohel <samuelmohel@gmail.com>
2023-08-25 00:34:41 +0100

    fix(interfaces): add paramlist in interface

---
+++ src/navigation/interfaces.ts
@@ -0,0 +1,8 @@
+export type IDepressiveEpisodesParamList = {
+    Home: undefined;
+    SymptomList: undefined;
+    SymptomChecker: undefined;
+    PossibleDiseases: undefined;
+    DetailedEpisodeInfo: undefined;
+    FavouriteEpisode: undefined;
+}
