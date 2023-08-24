0f1bac0adb56f64945d73a4e8d955dea884ec6b0
samuelmohel <samuelmohel@gmail.com>
2023-08-25 00:50:07 +0100

    feat(symptomchecker): create starting files for symptom checker screen

---
+++ src/screens/SymptomChecker/SymptomChecker.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+
+const SymptomChecker
+ = function SymptomChecker() {
+  return (
+    <View style={styles.container}>
+      <Text>SymptomChecker
+
+      </Text>
+    </View>
+  );
+};
+
+export default SymptomChecker
+;
+
+const styles = StyleSheet.create({
+  container: {}
+});
+
