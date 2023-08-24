4574da1b605a1409d55bfb777ec3f2221d5e77a3
samuelmohel <samuelmohel@gmail.com>
2023-08-25 00:51:15 +0100

    feat(symptomlist): create symptomlist screen starting files

---
+++ src/screens/SymptomList/SymptomList.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+interface SymptomListProps {}
+
+const SymptomList = function SymptomList() {
+  return (
+    <View style={styles.container}>
+      <Text>SymptomList
+    
+      </Text>
+    </View>
+  );
+};
+
+export default SymptomList;
+
+const styles = StyleSheet.create({
+  container: {}
+});
