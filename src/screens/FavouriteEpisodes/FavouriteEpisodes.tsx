8172c372916c8c4a101415fb3e69d6b41d5fb725
samuelmohel <samuelmohel@gmail.com>
2023-08-25 00:47:45 +0100

    feat(favouriteepisodes.tsx): create favourite episode starting files

---
+++ src/screens/FavouriteEpisodes/FavouriteEpisodes.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+interface FavouriteEpisodesProps {}
+
+const FavouriteEpisodes = function FavouriteEpisodes() {
+  return (
+    <View style={styles.container}>
+      <Text>FavouriteEpisodes
+        
+      </Text>
+    </View>
+  );
+};
+
+export default FavouriteEpisodes
+;
+
+const styles = StyleSheet.create({
+  container: {}
+});
