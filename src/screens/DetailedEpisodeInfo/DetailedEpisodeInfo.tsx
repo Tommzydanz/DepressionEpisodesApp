77ea2da669090f689247b68a0b0f9e7f756c5dc3
samuelmohel <samuelmohel@gmail.com>
2023-08-25 00:38:00 +0100

    feat(detailepisodeinfo): create starting file for detailed Episode info

---
+++ src/screens/DetailedEpisodeInfo/DetailedEpisodeInfo.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+
+const DetailedEpisodeInfo
+ = function DetailedEpisodeInfo () {
+  return (
+    <View style={styles.container}>
+      <Text>DetailedEpisodeInfo Screen
+      </Text>
+    </View>
+  );
+};
+
+export default DetailedEpisodeInfo
+;
+
+const styles = StyleSheet.create({
+  container: {}
+});
