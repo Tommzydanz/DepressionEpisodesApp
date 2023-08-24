b8c8eacb2b09ce5993c001ceb1b4fca355de5e82
samuelmohel <samuelmohel@gmail.com>
2023-08-25 00:48:44 +0100

    feat(PossibleDiseases): create possible diseases screen starting files

---
+++ src/screens/PossibleDiseases/PossibleDiseases.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+interface PossibleDiseasesProps {}
+
+const PossibleDiseases = function PossibleDiseases() {
+  return (
+    <View style={styles.container}>
+      <Text>PossibleDiseases
+s
+      </Text>
+    </View>
+  );
+};
+
+export default PossibleDiseases;
+
+const styles = StyleSheet.create({
+  container: {}
+});
