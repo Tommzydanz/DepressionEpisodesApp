ec3801ac7a155bc5fba270e3fcf9713222af827e
Tommzydanz <daniela.stomiwa@yahoo.com>
2024-01-02 03:59:20 +0100

    feat(interface): create interface for symptomquestions

---
+++ src/screens/SymptomQuestions/interfaces.ts
@@ -0,0 +1,16 @@
+import React from 'react';
+import {StackScreenProps} from '@react-navigation/stack';
+import {IDepressiveEpisodesParamList} from '../../navigation/interfaces';
+
+export type SymptomQuestion = {
+  ID: number;
+  Symptom: string;
+  Question: string;
+};
+
+export type SymptomQuestionsProp = React.FC<
+  StackScreenProps<IDepressiveEpisodesParamList, 'SymptomQuestions'> & {
+    data: SymptomQuestion[];
+    onAnswer: (answer: 'Yes' | 'No', questionId: number) => void;
+  }
+>;
