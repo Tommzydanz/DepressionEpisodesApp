9a644cf8033de6fbed74d2788d5ece8d352d82f3
Tommzydanz <daniela.stomiwa@yahoo.com>
2024-01-02 03:52:22 +0100

    fix(interface.ts); made changes on interface

--- src/components/Button/interfaces.ts
+++ src/components/Button/interfaces.ts
@@ -1,9 +1,8 @@
 import React from 'react';
-import { StyleProp } from 'react-native';
+import {StyleProp} from 'react-native';
 
 export type IButtonProps = React.FC<{
-      style: StyleProp<any>;
-      children: React.ReactNode;
-      onPress: () => void;
-    }
->;
+  style: StyleProp<any>;
+  children: React.ReactNode;
+  onPress: () => void;
+}>;
