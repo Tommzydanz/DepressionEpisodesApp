50c56194867732c697010f96776d5388c4db5a83
Tommzydanz <daniela.stomiwa@yahoo.com>
2024-01-02 03:57:09 +0100

    feat(favouriteepisides): new screen to save episodes

--- src/screens/FavouriteEpisodes/FavouriteEpisodes.tsx
+++ src/screens/FavouriteEpisodes/FavouriteEpisodes.tsx
@@ -1,21 +1,18 @@
 import * as React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import {Text, View, StyleSheet} from 'react-native';
 
 interface FavouriteEpisodesProps {}
 
 const FavouriteEpisodes = function FavouriteEpisodes() {
   return (
     <View style={styles.container}>
-      <Text>FavouriteEpisodes
-        
-      </Text>
+      <Text>FavouriteEpisodes</Text>
     </View>
   );
 };
 
-export default FavouriteEpisodes
-;
+export default FavouriteEpisodes;
 
 const styles = StyleSheet.create({
-  container: {}
+  container: {},
 });
